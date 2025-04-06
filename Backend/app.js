bd641914aeb005748e7e7595b8a93fd447001727
Ayush Jain <ayushj820@gmail.com>
2025-04-06 20:56:30 +0530

    Adding DB connection

--- Backend/app.js
+++ Backend/app.js
@@ -1,7 +1,11 @@
-import express from "express";
 import dotenv from 'dotenv';
 dotenv.config();
+
+import express from "express";
 import cors from 'cors';
+import { connectDB } from "./db/db.js";
+
+connectDB();
 
 const app = express();
 app.use(cors());
