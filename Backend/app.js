f8f802a863008263ebd891e4f6e021a051992e8d
Ayush Jain <ayushj820@gmail.com>
2025-04-06 20:37:12 +0530

    Initial Server Setup

---
+++ Backend/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from 'dotenv';
+dotenv.config();
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello Express!');
+});
+
+export {
+    app
+}
